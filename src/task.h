aedf3d55f76ac6a626ce9e79e08e494a6b45f9bc
xrfind <xrfind@gmail.com>
2015-04-27 17:21:58 +0200

    sort and task change

--- src/task.h
+++ src/task.h
@@ -1,10 +1,18 @@
 #ifndef TASK_H
 #define TASK_H 
 #include "metrics.h"
+#include "bip.h"
+#include "net.h"
 
 struct TASK {
 	struct METRICS * (*alg)(struct TASK *);
+	BIPS* train;
+	BIPS* test;
+	NETS* train_right;
+	int num_toprightused2cmptmetrics;
 
+	//used to improve efficient
+	int *rank;
 };
 
 #endif
